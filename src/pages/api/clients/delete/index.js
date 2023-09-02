3244ef1d9969aecce84dbd903193f89c26665591
Evandro <34420396+ev4ndropc@users.noreply.github.com>
2023-09-02 15:28:02 -0300

    Update index.js

--- src/pages/api/clients/delete/index.js
+++ src/pages/api/clients/delete/index.js
@@ -17,7 +17,7 @@ export default async function deleteClient(request, response) {
                 total_price = total_price + parseFloat(Number(client.price));
             }
         });
-        return response.status(200).json({ success: true, clients, total_price, deleteDomain });
+        return response.status(200).json({ success: true, clients, total_price });
     } catch (error) {
         return response.status(500).json({ error: error.message });
     }
